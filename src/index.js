2d2720c4fa9c1884b1b0db0f358224c101793737
Juan Rodriguez <jsrd98@gmail.com>
2018-07-07 22:17:56 -0500

    feat(Add Name option)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var uniqueRandomArray = require('unique-random-array');
-var costelog =  require('./costelog.json');
+var costelog = require('./costelog.json');
 
-var getRandomItem = uniqueRandomArray(costelog);
+var random = (name = "") => replaceName(name, uniqueRandomArray(costelog)());
+var all = (name = "") => costelog.map((item) => replaceName(name, item));
+
+function replaceName(name, item) {
+  return item.replace(/#NAME{(.*?)}/, name || '$1').trim();
+}
 
 module.exports = {
-  random: getRandomItem,
-  all: costelog
+  random,
+  all
 };
